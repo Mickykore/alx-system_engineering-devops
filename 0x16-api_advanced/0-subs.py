c44e5877d4bfea102a407c9c249afaac53b23f0e
mickykore <mikiyasayele6@gmail.com>
2023-05-09 21:43:56 +0300

    add file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,17 @@
 
 import requests
 
+"""Function to query subscribers on a given Reddit subreddit."""
+
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+            'User-Agent': 'Mozilla/5.0'
+            }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     else:
         return 0
+
