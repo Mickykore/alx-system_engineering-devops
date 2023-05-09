b112b0e49052795c737d0e1cfccfff720658696a
mickykore <mikiyasayele6@gmail.com>
2023-05-09 22:02:03 +0300

    add file 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts from a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
     'User-Agent': 'Mozilla/5.0'
     }
